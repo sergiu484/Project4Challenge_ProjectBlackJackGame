1de537bfdb99a3a0bbc493b827fc94fb497a8c8d
Ghimp Sergiu <ghimpsergiu@gmail.com>
2020-03-26 20:56:06 +0200

    Commit 7: Finish the Challenge 4: Change colors

--- script/script.js
+++ script/script.js
@@ -116,19 +116,62 @@ function rpsFrontEnd(humanImageChoise,botImageChoise,finalMessage){
  console.log(copyAllButtons);
 
  function buttonColorChange(buttonThingy){
-    if (buttonThingy === 'red'){
+    if (buttonThingy.value === 'red'){
         buttonRed();
-    }else if(buttonThingy === 'green'){
+    }else if(buttonThingy.value === 'green'){
         buttonGreen();
-    }else if(buttonThingy === 'reset'){
+    }else if(buttonThingy.value === 'reset'){
         buttonColorReset();
-    }else if(buttonThingy === 'random'){
+    }else if(buttonThingy.value === 'random'){
         randomColors();
     }
  }
 
  function buttonRed(){
      for(let i = 0; i < all_buttons.length; i++){
- 
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
      } 
  }
+ function buttonGreen(){
+    for(let i = 0; i < all_buttons.length; i++){
+       all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+       all_buttons[i].classList.add('btn-success');
+    } 
+}
+function buttonColorReset(){
+    for(let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+function randomColors(){
+    var choices = ['btn-danger','btn-primary','btn-success','btn-warning'];
+    for(let i = 0;i < all_buttons.length; i++){
+        var randomNumber = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
