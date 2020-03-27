2a10edcb2dfd39046fe9872cc70e2a9a0d0521e3
Ghimp Sergiu <ghimpsergiu@gmail.com>
2020-03-27 18:22:20 +0200

    Commit 13: Add to show card random in your box

--- script/script.js
+++ script/script.js
@@ -157,7 +157,8 @@ function randomColors(){
 /* Challeange 5: BlackJack*/
 let blackjackGame = {
     'you': {'scoreSpan':'your-blackJack-result','div':'#your-box','score':0},
-    'dealer': {'scoreSpan':'dealer-blackJack-result','div':'#dealer-box','score':0}
+    'dealer': {'scoreSpan':'dealer-blackJack-result','div':'#dealer-box','score':0},
+    'cards': ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 };
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
@@ -171,26 +172,40 @@ document.querySelector('#blackjack-stand-button').addEventListener('click',black
 document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackDeal);
 
 function blackjackHit(){
-    let cartImage = document.createElement('img');
-    cartImage.src = 'blackjackimages/Q.png';
-    document.querySelector(YOU['div']).appendChild(cartImage);
+    let card = randomCard();
+    console.log(card); 
+    showCard(card,YOU);
+}
+function randomCard(){
+    let randomIndex = Math.floor(Math.random()*13);
+    return blackjackGame['cards'][randomIndex];
+}
+function showCard(card,activePlayer){
+    let cardImage = document.createElement('img');
+    cardImage.src = `blackjackimages/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
 }
-
-function blackjackStand(){
-  //  alert("Charles click the Stand button");
-    standSound.play();
-}function blackjackDeal(){
-    //alert("Charles click the Deal button");
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');  
+    for(let i = 0; i < yourImages.length; i++){
+        yourImages[i].remove();
+    }
+    for(let i = 0; i < dealerImages.length; i++){
+        dealerImages[i].remove();
+    }
     dealSound.play();
 }
+function blackjackStand(){
+    //  alert("Charles click the Stand button");
+      standSound.play();
+  }
 
 
 
 
-
-
-
+  
 
 
 
